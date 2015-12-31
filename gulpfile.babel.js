bcaf90a880fc60f640ac6c57407a1118b53796b4
Ryan Murphy <rdmurphy@users.noreply.github.com>
2015-12-31 01:02:43 -0600

    Bring on the webpack and eslint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,104 +1,106 @@
-/* global -$ */
-'use strict';
-
 import gulp from 'gulp';
+import u from 'gulp-util';
 import gulpLoadPlugins from 'gulp-load-plugins';
 
-import autoprefixer from 'autoprefixer-core';
 import browserSync from 'browser-sync';
-import del from 'del';
-import buffer from 'vinyl-buffer';
-import runSequence from 'run-sequence';
-import source from 'vinyl-source-stream';
+import stripAnsi from 'strip-ansi';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
 import yargs from 'yargs';
-
-import browserify from 'browserify';
-import babelify from 'babelify';
-import watchify from 'watchify';
 
 const $ = gulpLoadPlugins();
 const args = yargs.argv;
 const bs = browserSync.create();
-const reload = bs.reload;
-const stream = bs.stream;
 
-function browserifyScripts(shouldWatch) {
-  let bundler = browserify('./{{ project_name }}/static_src/scripts/main.js', {
-    debug: true,
-  });
-
-  bundler.transform(babelify);
-
-  function bundle() {
-    return bundler.bundle()
-      .pipe(source('bundle.js'))
-      .pipe(buffer())
-      .pipe($.sourcemaps.init({loadMaps: true}))
-      .pipe($.if(args.production, $.uglify()))
-      .pipe($.sourcemaps.write(args.production ? '.' : undefined))
-      .pipe(gulp.dest('./{{ project_name }}/static/scripts/'))
-      .pipe(reload({stream: true}))
-      .pipe($.size({title: 'scripts'}));
-  }
-
-  if (shouldWatch) {
-    bundler = watchify(bundler);
-    bundler.on('update', bundle);
-    bundler.on('log', $.util.log);
-  }
-
-  return bundle();
+import webpackConfig from './webpack.config';
+if (args.production) {
+  webpackConfig.debug = false;
+  webpackConfig.devtool = 'source-map';
+  webpackConfig.plugins.push(
+    new webpack.optimize.UglifyJsPlugin()
+  );
 }
+const webpackBundle = webpack(webpackConfig);
 
-gulp.task('scripts', () => {
-  return browserifyScripts();
+webpackBundle.plugin('done', (stats) => {
+  if (stats.hasErrors() || stats.hasWarnings()) {
+    return bs.sockets.emit('fullscreen:message', {
+      title: 'Webpack Error:',
+      body: stripAnsi(stats.toString()),
+      timeout: 100000
+    });
+  }
+
+  bs.reload();
 });
 
-gulp.task('watchify', () => {
-  return browserifyScripts(true);
+gulp.task('scripts', (cb) => {
+  webpackBundle.run((err, stats) => {
+    if (err) throw new u.PluginError('webpack', err);
+    u.log('[webpack]', stats.toString({colors: true}));
+
+    cb();
+  });
 });
 
 gulp.task('styles', () => {
   return gulp.src('./{{ project_name }}/static_src/scss/*.scss')
+    .pipe($.newer('./{{ project_name }}/static/styles'))
     .pipe($.sourcemaps.init())
     .pipe($.sass({
       includePaths: ['node_modules'],
       precision: 10
     }).on('error', $.sass.logError))
-    .pipe($.postcss([
-      autoprefixer({
+    .pipe($.if(args.production, $.cssnano({
+      autoprefixer: {
         browsers: ['last 2 versions']
-      })
-    ]))
-    .pipe($.if(args.production, $.minifyCss({
-      keepSpecialComments: 0
+      },
+      discardComments: {
+        removeAll: true
+      }
     })))
     .pipe($.sourcemaps.write(args.production ? '.' : undefined))
     .pipe(gulp.dest('./{{ project_name }}/static/styles/'))
-    .pipe(stream({
-      match: '**/*.css'
-    }))
+    .pipe(bs.stream({match: '**/*.css'}))
     .pipe($.size({title: 'styles'}));
 });
 
-gulp.task('serve', ['styles', 'watchify'], () => {
+gulp.task('images', () => {
+  return gulp.src('./{{ project_name }}/static_src/images/**/*')
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('./{{ project_name }}/static/images/'))
+    .pipe($.size({title: 'images'}));
+});
+
+gulp.task('extras', () => {
+  return gulp.src(['./{{ project_name }}/static_src/favicon.ico', './{{ project_name }}/static_src/robots.txt'])
+    .pipe(gulp.dest('./{{ project_name }}/static'))
+    .pipe($.size({title: 'extras'}));
+});
+
+gulp.task('serve', ['images', 'extras', 'styles'], () => {
   bs.init({
+    logConnections: true,
     logPrefix: '{{ project_name }}'.toUpperCase(),
+    middleware: [
+      webpackDevMiddleware(webpackBundle, {
+        publicPath: webpackConfig.output.publicPath,
+        stats: {colors: true}
+      })
+    ],
     notify: false,
     open: false,
+    plugins: ['bs-fullscreen-message'],
     proxy: 'localhost:8000'
   });
 
   gulp.watch(['./{{ project_name }}/static_src/scss/**/*.scss'], ['styles']);
-  gulp.watch(['./{{ project_name }}/templates/**/*.html']).on('change', reload);
+  gulp.watch(['./{{ project_name }}/templates/**/*.html']).on('change', bs.reload);
 });
 
-gulp.task('clean', (cb) => {
-  return del(['./{{ project_name }}/static/styles/**/*'], cb);
-});
+gulp.task('default', ['images', 'extras', 'scripts', 'styles']);
 
 gulp.task('build', ['default']);
-
-gulp.task('default', ['clean'], cb => {
-  runSequence(['styles', 'scripts'], cb);
-});
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, '/{{ project_name }}/static_src/scripts'),
+  entry: {
+    main: './main.js'
+  },
+  output: {
+    path: path.join(__dirname, '/{{ project_name }}/static/scripts'),
+    publicPath: '/static/scripts',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json']
+  },
+  plugins: [],
+  debug: true,
+  devtool: 'cheap-module-eval-source-map'
+};
